b46f459fbb0ed11c5b39ac7186ae426ac6e308e6
tomalasustancia <tomalasustancia@gmail.com>
2026-09-25 20:19:08 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'sustama-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-mask-512.png', './icon-180.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first for the app shell so updates show up quickly; fall back to cache offline.
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
